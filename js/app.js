658db593f7a211b876bb178fd47007e1ee0502c3
talahajeer <tala.hajeer@gmail.com>
2021-02-09 15:34:17 +0200

    class3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+let score = 0;
 let userName = prompt("What's your name?");
 // console.log(userName);
 alert("Welcome to our website " + userName + " How about you know me better");
@@ -11,14 +12,15 @@ switch (qu1.toLowerCase()) {
     case 'y':
         // console.log(qu1.toLowerCase());
         alert("You are correct " + userName);
+        score++;
         break;
     case 'no':
     case 'n':
-         // console.log(qu1.toLowerCase());
+        // console.log(qu1.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-         // console.log(qu1.toLowerCase());
+        // console.log(qu1.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -27,16 +29,17 @@ let qu2 = prompt("Am I an Architect?");
 switch (qu2.toLowerCase()) {
     case 'yes':
     case 'y':
-         // console.log(qu2.toLowerCase());
+        // console.log(qu2.toLowerCase());
         alert("You are correct " + userName);
+        score++;
         break;
     case 'no':
     case 'n':
-         // console.log(qu2.toLowerCase());
+        // console.log(qu2.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-         // console.log(qu2.toLowerCase());
+        // console.log(qu2.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -45,16 +48,17 @@ let qu3 = prompt("Do I live in Irbid?");
 switch (qu3.toLowerCase()) {
     case 'yes':
     case 'y':
-         // console.log(qu3.toLowerCase());
+        // console.log(qu3.toLowerCase());
         alert("You are wrong " + userName);
         break;
     case 'no':
     case 'n':
-         // console.log(qu3.toLowerCase());
+        // console.log(qu3.toLowerCase());
         alert("You are correct " + userName);
+        score++;
         break;
     default:
-         // console.log(qu3.toLowerCase());
+        // console.log(qu3.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -62,16 +66,17 @@ let qu4 = prompt("Did I graduate Architecture from Univerity of Jordan?");
 switch (qu4.toLowerCase()) {
     case 'yes':
     case 'y':
-         // console.log(qu4.toLowerCase());
+        // console.log(qu4.toLowerCase());
         alert("You are correct " + userName);
+        score++;
         break;
     case 'no':
     case 'n':
-         // console.log(qu4.toLowerCase());
+        // console.log(qu4.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-         // console.log(qu4.toLowerCase());
+        // console.log(qu4.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -80,16 +85,61 @@ let qu5 = prompt("I am married, Do I have any children?");
 switch (qu5.toLowerCase()) {
     case 'yes':
     case 'y':
-         // console.log(qu5.toLowerCase());
+        // console.log(qu5.toLowerCase());
         alert("You are wrong " + userName);
         break;
     case 'no':
     case 'n':
-         // console.log(qu5.toLowerCase());
+        // console.log(qu5.toLowerCase());
         alert("You are correct " + userName);
+        score++;
         break;
     default:
-         // console.log(qu5.toLowerCase());
+        // console.log(qu5.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
+
+let qu6 = prompt("What was my degree out of 100 in highschool?");
+if (qu6 == 95) {
+    alert("You are correct " + userName);
+    score++;
+    // console.log(qu6.toLowerCase());
+} else {
+    for (let i = 0; (i < 4) && (qu6 != 95); i++) {
+        qu6 = prompt("Try Again " + userName);
+    }
+    if (qu6 == 95) {
+        alert("You are correct " + userName);
+        score++;
+        // console.log(qu6.toLowerCase());
+    } else {
+        prompt("Sorry Your out of attepmts the correct answer is 95%");
+        // console.log(qu6.toLowerCase());    
+    }
+}
+let visitedCoun = ["turkey", "malisia", "egypt", "lebanon"];
+
+let qu7 = prompt("Give it a guess, What countries I've been to?");
+
+if (visitedCoun.includes(qu7.toLowerCase())) {
+    alert("You are correct " + userName);
+    score++;
+    // console.log(qu7.toLowerCase());
+} else {
+    for (let i = 0; (i < 6) && (!visitedCoun.includes(qu7.toLowerCase())); i++) {
+        qu7 = prompt("Try Again " + userName);
+    }
+    if (visitedCoun.includes(qu7.toLowerCase())) {
+        alert("You are correct " + userName);
+        score++;
+        // console.log(qu7.toLowerCase());
+    } else {
+        prompt("Sorry Your out of attepmts the correct answer is " + visitedCoun);
+        // console.log(qu7.toLowerCase());
+    }
+
+}
+
+alert("well done your score is " + score + " out of 7");
+// console.log(score);
