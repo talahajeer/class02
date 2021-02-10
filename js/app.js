925f660312845d171e71728bd03062e52a310622
mohammad mahmoud mohammad hassouneh <mohammad.hs91@hotmail.com>
2021-02-10 16:03:00 +0200

    newchaiges

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use strict';
+
 let score = 0;
 let userName = prompt("What's your name?");
 // console.log(userName);
 alert("Welcome to our website " + userName + " How about you know me better");
 
 // switch checks the variable with many check cases
-
+function firstQ (){
 let qu1 = prompt("Am I 26 years old?");
 switch (qu1.toLowerCase()) {
     case 'yes':
@@ -24,7 +25,8 @@ switch (qu1.toLowerCase()) {
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
-
+} firstQ ();
+function secondQ (){
 let qu2 = prompt("Am I an Architect?");
 switch (qu2.toLowerCase()) {
     case 'yes':
@@ -43,7 +45,8 @@ switch (qu2.toLowerCase()) {
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
-
+} secondQ();
+function thirdQ (){
 let qu3 = prompt("Do I live in Irbid?");
 switch (qu3.toLowerCase()) {
     case 'yes':
@@ -62,6 +65,9 @@ switch (qu3.toLowerCase()) {
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
+}thirdQ();
+function furthQ(){
+
 let qu4 = prompt("Did I graduate Architecture from Univerity of Jordan?");
 switch (qu4.toLowerCase()) {
     case 'yes':
@@ -80,7 +86,8 @@ switch (qu4.toLowerCase()) {
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
-
+} furthQ();
+function fifthQ(){
 let qu5 = prompt("I am married, Do I have any children?");
 switch (qu5.toLowerCase()) {
     case 'yes':
@@ -99,7 +106,8 @@ switch (qu5.toLowerCase()) {
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
-
+}fifthQ();
+function sixthQ(){
 let qu6 = prompt("What was my degree out of 100 in highschool?");
 if (qu6 == 95) {
     alert("You are correct " + userName);
@@ -118,6 +126,7 @@ if (qu6 == 95) {
         // console.log(qu6.toLowerCase());    
     }
 }
+}sixthQ();
 
 // if (visitedCoun.includes(qu7.toLowerCase())) {
 //     alert("You are correct " + userName);
@@ -137,7 +146,7 @@ if (qu6 == 95) {
 //     }
 
 // }
-
+function sevenQ(){
 let visitedCoun = ["turkey", "malisia", "egypt", "lebanon"];
 let correct = 0;
 for (let index = 0; (index < 6); index++) {
@@ -164,7 +173,7 @@ if (correct == 1) {
     prompt("Sorry Your out of attepmts the correct answer is " + visitedCoun);
 }
 
-
+}sevenQ();
 // console.log(qu7.toLowerCase());
 
 alert("well done your score is " + score + " out of 7");
