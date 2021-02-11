241e7adc28daa9bdf4703b1f2fcd879bec7a7f25
talahajeer <tala.hajeer@gmail.com>
2021-02-11 15:04:10 +0200

    CSS

--- js/app.js
+++ js/app.js
@@ -11,17 +11,17 @@ let qu1 = prompt("Am I 26 years old?");
 switch (qu1.toLowerCase()) {
     case 'yes':
     case 'y':
-        // console.log(qu1.toLowerCase());
+        console.log(qu1.toLowerCase());
         alert("You are correct " + userName);
         score++;
         break;
     case 'no':
     case 'n':
-        // console.log(qu1.toLowerCase());
+        console.log(qu1.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-        // console.log(qu1.toLowerCase());
+        console.log(qu1.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -31,17 +31,17 @@ let qu2 = prompt("Am I an Architect?");
 switch (qu2.toLowerCase()) {
     case 'yes':
     case 'y':
-        // console.log(qu2.toLowerCase());
+        console.log(qu2.toLowerCase());
         alert("You are correct " + userName);
         score++;
         break;
     case 'no':
     case 'n':
-        // console.log(qu2.toLowerCase());
+        console.log(qu2.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-        // console.log(qu2.toLowerCase());
+        console.log(qu2.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -51,17 +51,17 @@ let qu3 = prompt("Do I live in Irbid?");
 switch (qu3.toLowerCase()) {
     case 'yes':
     case 'y':
-        // console.log(qu3.toLowerCase());
+        console.log(qu3.toLowerCase());
         alert("You are wrong " + userName);
         break;
     case 'no':
     case 'n':
-        // console.log(qu3.toLowerCase());
+        console.log(qu3.toLowerCase());
         alert("You are correct " + userName);
         score++;
         break;
     default:
-        // console.log(qu3.toLowerCase());
+        console.log(qu3.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -72,17 +72,17 @@ let qu4 = prompt("Did I graduate Architecture from Univerity of Jordan?");
 switch (qu4.toLowerCase()) {
     case 'yes':
     case 'y':
-        // console.log(qu4.toLowerCase());
+        console.log(qu4.toLowerCase());
         alert("You are correct " + userName);
         score++;
         break;
     case 'no':
     case 'n':
-        // console.log(qu4.toLowerCase());
+        console.log(qu4.toLowerCase());
         alert("You are wrong " + userName);
         break;
     default:
-        // console.log(qu4.toLowerCase());
+        console.log(qu4.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -92,17 +92,17 @@ let qu5 = prompt("I am married, Do I have any children?");
 switch (qu5.toLowerCase()) {
     case 'yes':
     case 'y':
-        // console.log(qu5.toLowerCase());
+        console.log(qu5.toLowerCase());
         alert("You are wrong " + userName);
         break;
     case 'no':
     case 'n':
-        // console.log(qu5.toLowerCase());
+        console.log(qu5.toLowerCase());
         alert("You are correct " + userName);
         score++;
         break;
     default:
-        // console.log(qu5.toLowerCase());
+        console.log(qu5.toLowerCase());
         alert("please " + userName + " answer with yes OR no only");
         break;
 }
@@ -112,7 +112,7 @@ let qu6 = prompt("What was my degree out of 100 in highschool?");
 if (qu6 == 95) {
     alert("You are correct " + userName);
     score++;
-    // console.log(qu6.toLowerCase());
+    console.log(qu6.toLowerCase());
 } else {
     for (let i = 0; (i < 4) && (qu6 != 95); i++) {
         qu6 = prompt("Try Again " + userName);
@@ -120,10 +120,10 @@ if (qu6 == 95) {
     if (qu6 == 95) {
         alert("You are correct " + userName);
         score++;
-        // console.log(qu6.toLowerCase());
+        console.log(qu6.toLowerCase());
     } else {
         prompt("Sorry Your out of attepmts the correct answer is 95%");
-        // console.log(qu6.toLowerCase());    
+        console.log(qu6.toLowerCase());    
     }
 }
 }sixthQ();
@@ -174,7 +174,7 @@ if (correct == 1) {
 }
 
 }sevenQ();
-// console.log(qu7.toLowerCase());
+console.log(qu7.toLowerCase());
 
 alert("well done your score is " + score + " out of 7");
-// console.log(score);
+console.log(score);
